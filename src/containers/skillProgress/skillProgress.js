1ff84b9e54aa7dd00a61bcd67025a5386f5f3479
AdnanDotNetDeveloper <ajarainn1432@gmail.com>
2025-04-25 00:27:20 -0700

    solve techstack secction progressbar bar issue

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Progress.scss";
 import {illustration, techStack} from "../../portfolio";
 import {Fade} from "react-awesome-reveal";
@@ -6,23 +6,34 @@ import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 
 export default function StackProgress() {
+  const progressRefs = useRef({});
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      techStack.experience.forEach((exp, i) => {
+        if (progressRefs.current[i]) {
+          progressRefs.current[i].style.width = exp.progressPercentage;
+        }
+      });
+    }, 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   if (techStack.viewSkillBars) {
     return (
-      <Fade bottom duration={1000} distance="20px">
+      <Fade direction="up" triggerOnce>
         <div className="skills-container">
           <div className="skills-bar">
             <h1 className="skills-heading">Proficiency</h1>
             {techStack.experience.map((exp, i) => {
-              const progressStyle = {
-                width: exp.progressPercentage
-              };
               return (
                 <div key={i} className="skill">
                   <p>{exp.Stack}</p>
                   <div className="meter">
-                    <Fade direction="left" triggerOnce>
-                      <span style={progressStyle}></span>
-                    </Fade>
+                    <span
+                      ref={el => (progressRefs.current[i] = el)}
+                      style={{ width: 0 }}
+                    ></span>
                   </div>
                 </div>
               );
--- src/portfolio.js
+++ src/portfolio.js
@@ -158,7 +158,7 @@ const techStack = {
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
